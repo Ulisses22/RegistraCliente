9bd571080c89af901cd291fcb2ce6098ed049cd6
Ulisses Alves <ulisses.alves@konicaminolta.pt>
2024-02-22 11:36:33 +0000

    some changes

--- registracliente/src/App.js
+++ registracliente/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route } from 'react-router-dom';
+import Clients from './clientes';
+import Products from './produtos';
+import Home from './home';
 
-
-const CadastroCliente = () => {
-    return (
-      
-        <div className="container">
-            <h1 className="mt-5">Bem-vindo à Loja Online</h1>
-            <div className="mt-3">
-                <Link to="/products" className="btn btn-primary m-1 mr-2">Ver Produtos</Link>
-                <Link to="/clients" className="btn btn-primary m-1">Ver Clientes</Link>
-            </div>
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/clientes" component={Clients} />
+      <Route path="/produtos" component={Products} />
+    </div>
+  );
 };
 
-export default CadastroCliente;
+export default App;
