73dbe0f8eb81677eb546a3901692d08808622e33
Ulisses Alves <ulisses.alves@konicaminolta.pt>
2024-04-06 19:28:02 +0100

    Update clientes.js

--- registracliente/src/clientes.js
+++ registracliente/src/clientes.js
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,16 +9,9 @@ const Cliente = () => {
     sexo: '',
     estadoCivil: '',
     cpf: '',
-    rg: '',
-    cep: '',
-    cidade: '',
     estado: '',
     telefone: '',
     email: '',
-    profissao: '',
-    empresa: '',
-    cargo: '',
-    renda: ''
   });
 
   const handleChange = (event) => {
@@ -35,7 +26,7 @@ const Cliente = () => {
     event.preventDefault();
 
     // Verifique se todos os campos obrigatórios foram preenchidos
-    const camposObrigatorios = ['nome', 'dataNascimento', 'sexo', 'estadoCivil', 'cpf', 'rg', 'cep', 'cidade', 'estado', 'telefone', 'email', 'profissao', 'empresa', 'cargo', 'renda'];
+    const camposObrigatorios = ['nome', 'dataNascimento', 'sexo', 'estadoCivil', 'cpf', 'estado', 'telefone', 'email'];
     const camposVazios = camposObrigatorios.filter(campo => !formData[campo]);
 
     if (camposVazios.length > 0) {
@@ -53,81 +44,94 @@ const Cliente = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="container mt-4"> {/* Adicione a classe 'container' para centralizar o formulário */}
-      <h2>Cadastro de Cliente</h2>
+    <div className="container mt-4 w-100">
       <div className="row">
-        <div className="col-md-6">
-          <div className="form-group">
-            <label>*Nome completo:</label>
-            <input type="text" name="nome" value={formData.nome} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Data de nascimento:</label>
-            <input type="date" name="dataNascimento" value={formData.dataNascimento} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Sexo:</label>
-            <select name="sexo" value={formData.sexo} onChange={handleChange} className="form-control" >
-              <option value="">Selecione</option>
-              <option value="masculino">Masculino</option>
-              <option value="feminino">Feminino</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>*Estado civil:</label>
-            <input type="text" name="estadoCivil" value={formData.estadoCivil} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*CPF:</label>
-            <input type="text" name="cpf" value={formData.cpf} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*RG:</label>
-            <input type="text" name="rg" value={formData.rg} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*CEP:</label>
-            <input type="text" name="cep" value={formData.cep} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Cidade:</label>
-            <input type="text" name="cidade" value={formData.cidade} onChange={handleChange} className="form-control"  />
-          </div>
+        <div className="col">
+          <form onSubmit={handleSubmit}>
+            <h2>Cadastro de Cliente</h2>
+            <div className="form-group">
+              <label>*Nome completo:</label>
+              <input type="text" name="nome" value={formData.nome} onChange={handleChange} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>*Data de nascimento:</label>
+              <input type="date" name="dataNascimento" value={formData.dataNascimento} onChange={handleChange} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>*Sexo:</label>
+              <select name="sexo" value={formData.sexo} onChange={handleChange} className="form-control">
+                <option value="">Selecione</option>
+                <option value="masculino">Masculino</option>
+                <option value="feminino">Feminino</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>*Estado civil:</label>
+              <input type="text" name="estadoCivil" value={formData.estadoCivil} onChange={handleChange} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>*CPF:</label>
+              <input type="text" name="cpf" value={formData.cpf} onChange={handleChange} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>*Estado:</label>
+              <input type="text" name="estado" value={formData.estado} onChange={handleChange} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>*Telefone:</label>
+              <input type="text" name="telefone" value={formData.telefone} onChange={handleChange} className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>*E-mail:</label>
+              <input type="email" name="email" value={formData.email} onChange={handleChange} className="form-control" />
+            </div>
+            <button type="submit" className="btn btn-primary mt-3 w-100">Cadastrar</button>
+          </form>
         </div>
-        <div className="col-md-6">
-          <div className="form-group">
-            <label>*Estado:</label>
-            <input type="text" name="estado" value={formData.estado} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Telefone:</label>
-            <input type="text" name="telefone" value={formData.telefone} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*E-mail:</label>
-            <input type="email" name="email" value={formData.email} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Profissão:</label>
-            <input type="text" name="profissao" value={formData.profissao} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Empresa:</label>
-            <input type="text" name="empresa" value={formData.empresa} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Cargo:</label>
-            <input type="text" name="cargo" value={formData.cargo} onChange={handleChange} className="form-control"  />
-          </div>
-          <div className="form-group">
-            <label>*Renda:</label>
-            <input type="text" name="renda" value={formData.renda} onChange={handleChange} className="form-control"  />
-          </div>
-          <p class=""><strong>* Campos obrigatórios</strong></p>
+        <div className="col">
+          <h2>Lista de Clientes</h2>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Data de Nascimento</th>
+                <th>Sexo</th>
+                <th>Estado Civil</th>
+                <th>CPF</th>
+                <th>Estado</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>João</td>
+                <td>01/01/1990</td>
+                <td>Masculino</td>
+                <td>Solteiro</td>
+                <td>123.456.789-00</td>
+                <td>São Paulo</td>
+                <td>(11) 1234-5678</td>
+                <td>joao@example.com</td>
+                <td>
+                  <button type="button" class="btn btn-primary btn-sm mr-1">
+                    <span class="mr-1">Editar</span>
+                    <i class="fas fa-pencil-alt"></i>
+                  </button>
+                  <button type="button" class="btn btn-danger btn-sm">
+                    <span class="mr-1">Excluir</span>
+                    <i class="fas fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+
         </div>
       </div>
-      <button type="submi" className="btn btn-primary mt-3">Cadastrar</button> {/* Adicione a classe "btn" e "btn-primary" para estilizar o botão */}
-    </form>
+    </div>
   );
 };
 
